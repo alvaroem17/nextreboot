37850ccfa3bf7e41e90bd57fb24b8cfc0368abfb
Alvaro Estévez <alvaro5estevez@gmail.com>
2023-11-22 16:41:25 +0000

    Signup and Login styles, authNav without functionality

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,6 +1,6 @@
 import AuthNav from "@/components/authNav/authNav";
 import styles from "./login.module.css"
-import { Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 export default function Login() {
   return (
@@ -8,6 +8,15 @@ export default function Login() {
       <AuthNav />
       <main className={styles.main}>
         <Typography variant="h1" className="title">Iniciar sesión</Typography>
+        <form className={styles.form}>
+          <TextField id="outlined-basic" label="Email" variant="outlined" type="email"/>
+          <TextField id="outlined-basic" label="Contraseña" variant="outlined" type="password"/>
+          <Box sx={{ display: 'flex', gap: '5px', justifyContent: 'end', marginTop: '1rem'}}>
+            <Button variant="contained" sx={{ backgroundColor: '#AF0FEC', color:'white'}}>Aceptar</Button>
+            <Button variant="contained" color="error">Cancelar</Button>
+          </Box>
+            
+        </form>
       </main>
     </>
   )
--- src/components/authNav/authNav.js
+++ src/components/authNav/authNav.js
@@ -9,7 +9,6 @@ import { usePathname } from 'next/navigation';
 
 export default function AuthNav() {
     const currentRoute = usePathname()
-    console.log(currentRoute)
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: 'transparent', color: 'Black', boxShadow: 'none', padding: '10px 0'}}>
@@ -21,7 +20,7 @@ export default function AuthNav() {
           >
             Ylenia Estévez
           </Typography>
-          <Button sx={{ backgroundColor: '#AF0FEC', color:'white', borderRadius: '4px', }}>{ currentRoute === "/Login" ? "Crear cuenta" : "Iniciar sesión"}</Button>
+          <Button href={ currentRoute === "/Login" ? "/Signup" : "/Login"} sx={{ backgroundColor: '#AF0FEC', color:'white', borderRadius: '4px', }}>{ currentRoute === "/Login" ? "Crear cuenta" : "Iniciar sesión"}</Button>
         </Toolbar>
       </AppBar>
     </Box>
