d5622a267314740c1bfeb1a7f785691d3fb59430
Alvaro Estévez <alvaro5estevez@gmail.com>
2023-11-24 11:43:36 +0000

    Mobile view completed-> adjust navbar depends on the autorization

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,9 +1,11 @@
 
+import NavBar from '@/components/navBar/navBar'
 import './../globals.css'
 
 export default function HomeLayout({ children }) {
   return (
     <>
+        <NavBar/>
         {children}
     </>
   )
